3c88bac80ff4e4043fc4b8f2e0c73cfa6224f5d8
Himanshu Sinha <ashusinha1904@gmail.com>
2024-10-10 15:49:04 +0530

    home page UI updated

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,36 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import tw from "twrnc";
+import {
+  AntDesign,
+  FontAwesome5,
+  Ionicons,
+  Octicons,
+} from "@expo/vector-icons";
+
+import { homeMenu } from "@/constants/home-menu";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView style={tw`bg-white pt-5 px-4`}>
+      <View style={tw`flex-row justify-between`}>
+        <Text style={tw`font-medium text-base`}>My Work</Text>
+        <AntDesign name="ellipsis1" size={24} color="gray" />
+      </View>
+
+      <View style={tw`mt-7 gap-y-7`}>
+        {homeMenu.map((item) => {
+          return (
+            <View style={tw`flex-row gap-x-4 items-center`} key={item.title}>
+              <View style={tw`bg-[${item.color}] py-2 px-2 rounded-md`}>
+                <item.Icon name={item.iconName} size={16} color="white" />
+              </View>
+              <Text style={tw`text-base`}>{item.title}</Text>
+            </View>
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 };
 
---
+++ constants/home-menu.ts
@@ -0,0 +1,52 @@
+import {
+  AntDesign,
+  FontAwesome5,
+  Ionicons,
+  MaterialIcons,
+  Octicons,
+} from "@expo/vector-icons";
+
+export const homeMenu = [
+  {
+    title: "Issues",
+    Icon: FontAwesome5,
+    iconName: "dot-circle",
+    color: "#22c55e",
+  },
+  {
+    title: "Pull Requests",
+    Icon: Ionicons,
+    iconName: "git-pull-request",
+    color: "#2563eb",
+  },
+  {
+    title: "Discussions",
+    Icon: Octicons,
+    iconName: "comment-discussion",
+    color: "#7e22ce",
+  },
+  {
+    title: "Projects",
+    Icon: MaterialIcons,
+    iconName: "window",
+    color: "#6b7280",
+  },
+  {
+    title: "Repositories",
+    Icon: AntDesign,
+    iconName: "book",
+    color: "#1f2937",
+  },
+  {
+    title: "Organizations",
+    Icon: Octicons,
+    iconName: "organization",
+    color: "#ea580c",
+  },
+  {
+    title: "Starred",
+    Icon: AntDesign,
+    iconName: "staro",
+    color: "#facc15",
+  },
+];
