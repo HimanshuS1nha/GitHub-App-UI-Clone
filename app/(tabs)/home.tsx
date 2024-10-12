699c0a0aa62f4128f42054956185d7b96c3a113c
Himanshu Sinha <ashusinha1904@gmail.com>
2024-10-12 22:33:10 +0530

    Home page UI complete

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,18 +1,14 @@
 import { View, Text, ScrollView, Pressable } from "react-native";
 import React from "react";
 import tw from "twrnc";
-import {
-  AntDesign,
-  FontAwesome5,
-  Ionicons,
-  Octicons,
-} from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 import { homeMenu } from "@/constants/home-menu";
+import { shortCutIcons } from "@/constants/shortcut-icons";
 
 const Home = () => {
   return (
-    <ScrollView contentContainerStyle={tw`bg-white py-5`}>
+    <ScrollView contentContainerStyle={tw`bg-white py-4`}>
       <View style={tw`flex-row justify-between px-4`}>
         <Text style={tw`font-medium text-base`}>My Work</Text>
         <AntDesign name="ellipsis1" size={24} color="gray" />
@@ -31,25 +27,65 @@ const Home = () => {
         })}
       </View>
 
-      <View style={tw`h-[0.2px] bg-gray-400 mt-7`} />
+      <View style={tw`h-[0.4px] bg-gray-400 mt-7`} />
 
-      <View style={tw`px-4 mt-7 gap-y-6`}>
+      <View style={tw`px-4 mt-7 gap-y-7`}>
         <Text style={tw`font-medium text-base`}>Favourties</Text>
-        <View style={tw`items-center`}>
-          <Text style={tw`text-gray-700 text-center w-[90%] leading-5 text-base`}>
-            Add favourite repositories for quick access at any time, without
-            having to search
-          </Text>
-        </View>
+        <View style={tw`gap-y-4`}>
+          <View style={tw`items-center`}>
+            <Text
+              style={tw`text-gray-700 text-center w-[90%] leading-5 text-base`}
+            >
+              Add favourite repositories for quick access at any time, without
+              having to search
+            </Text>
+          </View>
 
-        <Pressable
-          style={tw`items-center justify-center shadow-sm shadow-black h-12`}
-        >
-          <Text style={tw`text-blue-600 font-medium`}>ADD FAVOURITES</Text>
-        </Pressable>
+          <Pressable
+            style={tw`items-center justify-center shadow shadow-gray-400 h-12`}
+          >
+            <Text style={tw`text-blue-600 font-medium`}>ADD FAVOURITES</Text>
+          </Pressable>
+        </View>
       </View>
 
-      <View style={tw`h-[0.2px] bg-gray-400 mt-7`} />
+      <View style={tw`h-[0.4px] bg-gray-400 mt-7`} />
+
+      <View style={tw`px-4 mt-7 gap-y-7`}>
+        <Text style={tw`font-medium text-base`}>Shortcuts</Text>
+
+        <View style={tw`gap-y-4`}>
+          <View style={tw`items-center gap-y-2`}>
+            <View style={tw`flex-row items-center gap-x-1`}>
+              {shortCutIcons.map((item) => {
+                return (
+                  <View
+                    key={item.iconName}
+                    style={tw`p-1.5 bg-[${item.bgColor}] rounded-full`}
+                  >
+                    <item.Icon
+                      name={item.iconName}
+                      color={item.color}
+                      size={16}
+                    />
+                  </View>
+                );
+              })}
+            </View>
+            <Text style={tw`font-semibold text-base`}>
+              The things you need, one tap away
+            </Text>
+            <Text style={tw`text-gray-700 text-base text-center w-[88%]`}>
+              Fast access your list of Issues, Pull Requests, or Discussions
+            </Text>
+          </View>
+          <Pressable
+            style={tw`items-center justify-center shadow shadow-gray-400 h-12`}
+          >
+            <Text style={tw`text-blue-600 font-medium`}>GET STARTED</Text>
+          </Pressable>
+        </View>
+      </View>
     </ScrollView>
   );
 };
---
+++ constants/shortcut-icons.ts
@@ -0,0 +1,59 @@
+import {
+  Entypo,
+  Feather,
+  FontAwesome5,
+  Ionicons,
+  MaterialCommunityIcons,
+  Octicons,
+} from "@expo/vector-icons";
+
+export const shortCutIcons = [
+  {
+    color: "#000000",
+    bgColor: "#e5e7eb",
+    Icon: MaterialCommunityIcons,
+    iconName: "lightning-bolt-outline",
+  },
+  {
+    color: "#16a34a",
+    bgColor: "#dcfce7",
+    Icon: FontAwesome5,
+    iconName: "dot-circle",
+  },
+  {
+    color: "#2563eb",
+    bgColor: "#dbeafe",
+    Icon: Ionicons,
+    iconName: "git-pull-request",
+  },
+  {
+    color: "#9333ea",
+    bgColor: "#f3e8ff",
+    Icon: Octicons,
+    iconName: "comment-discussion",
+  },
+  {
+    color: "#ea580c",
+    bgColor: "#ffedd5",
+    Icon: Octicons,
+    iconName: "organization",
+  },
+  {
+    color: "#db2777",
+    bgColor: "#fce7f3",
+    Icon: Feather,
+    iconName: "users",
+  },
+  {
+    color: "#7c3aed",
+    bgColor: "#ede9fe",
+    Icon: Ionicons,
+    iconName: "bag-outline",
+  },
+  {
+    color: "#000000",
+    bgColor: "#e5e7eb",
+    Icon: Feather,
+    iconName: "file-plus",
+  },
+];
